21b1a892135f6b42cff7e8b26ab2fd2e56049f25
MRLamb <lamb.matthew@gmail.com>
2016-09-08 19:57:57 -0500

    Initial commit, assignment for class.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using ParagraphParserIntoDictionary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pp = new ParagraphParser("On old olympus, a man sat and sat. An old man was he and he sang to the trees.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ParagraphParserIntoDictionary/ParagraphParser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParagraphParserIntoDictionary
+{
+    public class ParagraphParser : Dictionary<string, List<String>>
+    {
+        public ParagraphParser(string WallOfText)
+        {
+            string[] split = WallOfText.Split(new char[] { ',', '!', ' ', '.' },
+                StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < split.Length; i++)
+            {
+                if (!this.ContainsKey(split[i]))
+                {
+                    this.Add(split[i], new List<String>());
+                    if (!(i + 1 == split.Length))
+                        this[split[i]].Add(split[i + 1]);
+                }
+                else
+                {
+                    if (!(i + 1 == split.Length))
+                        this[split[i]].Add(split[i + 1]);
+
+                }
+            }
+        }
+    }
+}
